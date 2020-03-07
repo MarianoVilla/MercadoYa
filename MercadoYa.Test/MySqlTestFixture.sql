557c58bb3ea6e5eeb44e1fc7b9698cc56c9bc09b
Mariano Villa <Mariano.Villa@outlook.com.ar>
2020-03-06 22:58:14 -0300

    [Back-end] MySql tests and a few bug fixes.

--- MercadoYa.Test/MySqlTestFixture.sql
+++ MercadoYa.Test/MySqlTestFixture.sql
@@ -5,19 +5,22 @@ CALL tap.no_plan();
 
 -- TruncateAll
 CALL TRUNCATEALL();
-SELECT tap.ok((SELECT COUNT(*) FROM users) = 0, "No users after TruncateAll.");
-SELECT tap.ok((SELECT COUNT(*) FROM tags) = 0, "No tags after TruncateAll.");
+SELECT tap.eq((SELECT COUNT(*) FROM users), 0, "No users after TruncateAll.");
+SELECT tap.eq((SELECT COUNT(*) FROM tags), 0, "No tags after TruncateAll.");
+SELECT tap.eq((SELECT COUNT(*) FROM tag_foods), 0, "No foods after TruncateAll.");
+SELECT tap.eq((SELECT COUNT(*) FROM user_credentials), 0, "No user_credentials after TruncateAll.");
+SELECT tap.eq((SELECT COUNT(*) FROM users_tags), 0, "No users_tags after TruncateAll.");
+
 
 -- RandomEntities
 CALL CreateRandomEntities(10);
-SELECT tap.ok((SELECT COUNT(*) FROM users) = 20, "Should create 20 random users.");
-SELECT tap.ok((SELECT COUNT(*) FROM store_users) = 10, "10 of the users should be store users.");
-SELECT tap.ok((SELECT COUNT(*) FROM customer_users) = 10, "10 of the users should be customer users.");
+SELECT tap.eq((SELECT COUNT(*) FROM users), 20, "Should create 20 random users.");
+SELECT tap.eq((SELECT COUNT(*) FROM store_users), 10, "10 of the users should be store users.");
+SELECT tap.eq((SELECT COUNT(*) FROM customer_users), 10, "10 of the users should be customer users.");
+SELECT tap.eq((SELECT COUNT(*) FROM users_tags), 20, "Should create 1 user/tag relation for each user");
+SELECT tap.eq((SELECT COUNT(*) FROM tags), 10, "Sould create 10 tags.");
+SELECT tap.eq((SELECT COUNT(*) FROM tag_foods), 10, "All 10 tags should be foods.");
 
--- 
-CALL InsertFood('Name', 'Description', 'Region', @Id);
-SELECT tap.ok(@Id IS NOT NULL, 'Should instert a food');
-SELECT tap.ok((SELECT COUNT(*) FROM tags WHERE id = @Id) = 1, 'The inserted food should be in tags.');
 
 CALL tap.finish();
 
